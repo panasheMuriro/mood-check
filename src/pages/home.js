4b43a0d75c4cf1b8c1367ab9618f40e5a3b01cc1
murirop <panashemuriro2023@u.northwestern.edu>
2023-02-16 21:52:30 -0600

    fix

--- src/pages/home.js
+++ src/pages/home.js
@@ -56,9 +56,9 @@ export default function Home() {
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#F8CBA6" fill-opacity="1" d="M0,256L34.3,245.3C68.6,235,137,213,206,208C274.3,203,343,213,411,213.3C480,213,549,203,617,170.7C685.7,139,754,85,823,101.3C891.4,117,960,203,1029,202.7C1097.1,203,1166,117,1234,96C1302.9,75,1371,117,1406,138.7L1440,160L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z"></path></svg>
 
       <div style={{ padding: 20, height: "90vh" }}>
-        <h2 style={{ color: "#808080", }}>How are you feeling?</h2>
+        <h2 style={{ color: "#808080", margin: 0}}>How are you feeling?</h2>
 
-        <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", gridTemplateRows: "1fr 1fr 1fr", flexWrap: "wrap", justifyContent: "center", width:"100%", height: 200 }}>
+        <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gridTemplateRows: "1fr 1fr 1fr", flexWrap: "wrap", justifyContent: "center", width:"100%", height: 200 }}>
           {emojis.map((x, index) =>
             <button key={index} onClick={() => setSelectedEmoji(x)} style={{ fontSize: 60, backgroundColor: "#FEFBE9", textAlign: "center", borderRadius: 10, borderColor: "#F8CBA6", borderStyle: "solid", borderWidth: 2 }}>{x}</button>
           )}
